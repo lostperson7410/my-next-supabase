9ca48e28eb20c2cc35b2c4b3369dfe6000f54855
lostperson7410 <55078251+lostperson7410@users.noreply.github.com>
2024-02-25 00:54:47 +0700

    Redux Added and Add UserSilce

--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -1,14 +1,29 @@
 'use client';
 
 import { supabase } from '@/utils/supabase/supabaseClient';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { registerUser } from './formAction/registerAction';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUser, setUser } from '@/lib/features/userProfile/userProfileSlice';
 
 export default function RegisterForm() {
 
+    const dispatch = useDispatch()
+    const userRudex = useSelector(selectUser)
+
+    const handleSubmit = async (e :FormData) => {
+        const response = await registerUser(e)
+        dispatch(setUser(response?.user))
+    }
+
+    useEffect(() => {
+        console.log("userRudex: ",userRudex);
+        
+    }, [userRudex]);
+
     return (
         <div className=' ml-2 mr-2 flex-1 flex flex-col w-full'>
-            <form action={registerUser} className='flex flex-col gap-2'>
+            <form action={handleSubmit} className='flex flex-col gap-2'>
                     <input
                     className=' bg-white text-slate-900 rounded-sm'
                         name='fullname'
--- components/formAction/registerAction.ts
+++ components/formAction/registerAction.ts
@@ -1,8 +1,12 @@
 'use server'
 
+import { setUser } from "@/lib/features/userProfile/userProfileSlice";
 import { createClient } from "@/utils/supabase/server";
+    
+import { useDispatch } from 'react-redux';
 
 const supabase = createClient()
+
 
 export const registerUser = async (formDate : FormData) =>{
     const {data} = await supabase.auth.getUser()
@@ -23,9 +27,4 @@ export const registerUser = async (formDate : FormData) =>{
     // }).eq('id',data?.user?.id).select()
     const res = await supabase.auth.updateUser({data:{username: username,full_name: fullname}})
     return res?.data
-
-
-
-
-
 }
