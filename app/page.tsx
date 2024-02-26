cb3330265b2725c0c9af3988e86943738b4a5db4
lostperson7410 <55078251+lostperson7410@users.noreply.github.com>
2024-02-26 23:24:27 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { createClient } from "@/utils/supabase/server";
 import Header from "@/components/Header";
 import RegisterForm from "@/components/Form/RegisterForm";
 import SignInSignUpForm from "@/components/Form/SignInSignUpForm";
+import AuthButton from "@/components/AuthButton";
 
 export default async function Index() {
   const supabase = createClient();
@@ -13,11 +14,8 @@ export default async function Index() {
   return (
     <div>
       <Header />
-      <p>this is user Meta data</p>
-      <p>username: {data?.user?.user_metadata?.username}</p>
-      <p>fullname: {data?.user?.user_metadata?.full_name}</p>
       <h1>Welcome New user</h1>
-      <SignInSignUpForm/>
+      <AuthButton/>
     </div>
   );
 }
